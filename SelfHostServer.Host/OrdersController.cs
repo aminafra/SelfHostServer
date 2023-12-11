95216590a21391a654b482d7a31622fa4c3eee8f
aminafra <mohamadamin.afrasiabi@gmail.com>
2023-12-11 22:53:43 +0330

    Init SelfHost Project

---
+++ SelfHostServer.Host/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostServer.Host
+{
+    public class OrdersController : ApiController
+    {
+        Order[] orders = new Order[]
+        {
+            new Order { Id = 1, Name = "order 1", Category = "test category1", Price = 1 },
+            new Order { Id = 2, Name = "order 2", Category = "test category2", Price = 20 },
+            new Order { Id = 3, Name = "order 3", Category = "test category3", Price = 300 },
+            new Order { Id = 4, Name = "order 4", Category = "test category4", Price = 4000 },
+        };
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return orders;
+        }
+    }
+}
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ SelfHostServer.Host/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace SelfHostServer.Host
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "product 1", },
+            new Product { Id = 2, Name = "product 2", },
+            new Product { Id = 3, Name = "product 3", },
+        };
+        
+        [HttpGet]
+        public IEnumerable<Product> All()
+        {
+            return products;
+        }
+    }
+}
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
